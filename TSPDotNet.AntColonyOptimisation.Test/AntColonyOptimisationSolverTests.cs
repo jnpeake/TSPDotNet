1273c2d66f699fffc21b0431976b31e82ec7353e
Josh Peake <joshpeake@live.com>
2025-08-07 20:55:49 +0100

    Add test project and AntDirector class - The AntDirector will manage the individual ants

---
+++ TSPDotNet.AntColonyOptimisation.Test/AntColonyOptimisationSolverTests.cs
@@ -0,0 +1,19 @@
+﻿using TSPDotNet.Domain;
+using AutoFixture;
+
+namespace TSPDotNet.AntColonyOptimisation.Service.Test;
+public class AntColonyOptimisationSolverTests
+{
+    [Fact]
+    public void GivenSolverIsProvidedWithProblem_WhenSolveIsCalled_ThenSolutionRouteIsReturned()
+    {
+        var fixture = new Fixture();
+        var solver = new AntColonyOptimisationSolver();
+        var problem = fixture.Create<Problem>();
+
+        var result = solver.Solve(problem);
+
+        Assert.NotNull(result);
+        Assert.IsType<SolutionRoute>(result);
+    }
+}
--- TSPDotNet.Service/AntColonyOptimisationSolver.cs
+++ TSPDotNet.Service/AntColonyOptimisationSolver.cs
@@ -4,5 +4,8 @@ namespace TSPDotNet.AntColonyOptimisation.Service;
 
 public class AntColonyOptimisationSolver : ISolver
 {
-
+    public SolutionRoute Solve(Problem inputProblem)
+    {
+        return new SolutionRoute(new List<int> { 0, 1, 2}, 1.23m);
+    }
 }
